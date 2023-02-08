29c3f17f756ce0f098f0c763701ff54a778f8623
Kirimarchy <92118397+Kirimarchy@users.noreply.github.com>
2023-02-08 18:42:26 +0300

    SavedMovies search fixed

--- src/components/pages/SavedMovies/SavedMovies.js
+++ src/components/pages/SavedMovies/SavedMovies.js
@@ -8,22 +8,17 @@ import { filterByDuration, filterByQuery } from "../../../utils/utils";
 const SavedMovies = () => {
   const { currentUser, userMovies } = useContext(CurrentUserContext);
   const [isFilter, setFilter] = useState(false);
-  const [isNotFound, setNotFound] = useState(false);
   const [initialMovies, setInitialMovies] = useState(userMovies);
+
+  const isNotFound = initialMovies.length === 0;
 
   useEffect(()=>{
     setInitialMovies( userMovies );
-    console.log('1111111111');
   }, [userMovies.length, currentUser])
   
   function submitSearchQuery(query){
     const moviesList = filterByQuery(userMovies, query);
-    if (moviesList.length === 0) {
-      setNotFound(true);
-    } else {
-      setNotFound(false);
-      setInitialMovies(moviesList);
-    }
+    setInitialMovies(moviesList);
     console.log('query', userMovies, query, moviesList, initialMovies);
   }
   
