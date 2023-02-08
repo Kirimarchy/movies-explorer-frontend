0e1011bf1e020618788bc589ed292969c28a642f
Kirimarchy <92118397+Kirimarchy@users.noreply.github.com>
2023-02-08 10:00:48 +0300

    Update SavedMovies.js
    fixed refresh page bug

--- src/components/pages/SavedMovies/SavedMovies.js
+++ src/components/pages/SavedMovies/SavedMovies.js
@@ -6,12 +6,15 @@ import CurrentUserContext from "../../../utils/context/CurrentUserContext";
 import { filterByDuration, filterByQuery } from "../../../utils/utils";
 
 const SavedMovies = () => {
-  const { userMovies } = useContext(CurrentUserContext);
+  const { currentUser, userMovies } = useContext(CurrentUserContext);
   const [isFilter, setFilter] = useState(false);
   const [isNotFound, setNotFound] = useState(false);
   const [initialMovies, setInitialMovies] = useState(userMovies);
-  const [moviesFiltered, setMoviesFiltered] = useState(userMovies);
 
+  useEffect(()=>{
+    setInitialMovies( userMovies );
+  }, [userMovies.length, currentUser])
+  
   function submitSearchQuery(query){
     const moviesList = filterByQuery(userMovies, query);
     if (moviesList.length === 0) {
@@ -19,28 +22,12 @@ const SavedMovies = () => {
     } else {
       setNotFound(false);
       setInitialMovies(moviesList);
-      // setMoviesFiltered(moviesList);
     }
     console.log('query', userMovies, query, moviesList);
   }
   
   function onChangeFilter(){setFilter(!isFilter)};  
-  //   if (isFilter) {
-  //     setMoviesFiltered(filterByDuration(initialMovies));
-  //   } else {
-  //     setMoviesFiltered(initialMovies);
-  //   }
-  //   setNotFound(moviesFiltered.length === 0 ? true : false );
-  // }
 
-  // useEffect(() => {
-  //    setInitialMovies(userMovies);
-  //    setMoviesFiltered( isFilter ? filterByDuration(userMovies) : userMovies );
-  // }, [userMovies.length, isFilter]);
-
-  // function closePopUp() {
-  //   setPopUp({ ...isPopUp, isOpen: false });
-  // }
 
   return (
     <main className="saved-movies">
